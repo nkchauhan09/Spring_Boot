a5d8b3d1342105531bf2d438da8b8228ec614448
Neeraj Kumar Chauhan <kneeraj460@gmail.com>
2021-03-01 23:23:24 +0530

    First Commit

---
+++ HelloWorld/src/main/java/com/SpringBoot/Controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.SpringBoot.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	public String hello()
+	{
+		return "Hello World";
+	}
+
+}
