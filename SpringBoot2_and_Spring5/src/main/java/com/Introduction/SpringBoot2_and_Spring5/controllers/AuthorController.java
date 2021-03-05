d091ab02f07fd19408c2f59524fb0a92020dfa79
Neeraj Kumar Chauhan <kneeraj460@gmail.com>
2021-03-05 11:37:50 +0530

    Final Commit

---
+++ SpringBoot2_and_Spring5/src/main/java/com/Introduction/SpringBoot2_and_Spring5/controllers/AuthorController.java
@@ -0,0 +1,26 @@
+package com.Introduction.SpringBoot2_and_Spring5.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.Introduction.SpringBoot2_and_Spring5.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+
+	private final AuthorRepository authorRepository;
+
+	public AuthorController(AuthorRepository authorRepository) {
+		this.authorRepository = authorRepository;
+	}
+	
+	@RequestMapping("/authors")
+	public String getAuthors(Model model)
+	{
+		model.addAttribute("authors", authorRepository.findAll());
+		return "authors/list";
+	}
+	
+	
+}
