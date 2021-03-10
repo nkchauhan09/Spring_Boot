7bbb0086efb4e5cf4c7c7054cfc0193e7f447e0f
Neeraj Kumar Chauhan <kneeraj460@gmail.com>
2021-03-10 22:33:13 +0530

    Unit Test for Controller

---
+++ testController/src/test/java/com/mockito/testController/TestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockito.testController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
